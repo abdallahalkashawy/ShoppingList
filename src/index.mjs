5b2efb146f96f847eb22132968aff69a7e74f3a9
Abdallah Ehab <abdallah.alkashawy@gmail.com>
2024-06-07 20:34:31 +0300

    Add & Update Product

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
+// middleware to parse json data
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 const products = [
@@ -9,6 +12,8 @@ const products = [
     {id : 2 , productName : 'product 2' , quantityAvailable : 200 , price : 400},
     {id : 3 , productName : 'product 3' , quantityAvailable : 300 , price : 500},
 ];
+
+//filter products by query parameters
 
 app.get("/api/v1/products", (req, res) => {
     console.log(req.query);
@@ -30,6 +35,8 @@ app.get("/api/v1/products", (req, res) => {
     res.json(products);
 });
 
+// get specific product by id parameter
+
 app.get("/api/v1/products/:id" , (req, res) => {
     const id = req.params.id;
     const product = products.find((product) => product.id === parseInt(id));
@@ -41,6 +48,65 @@ app.get("/api/v1/products/:id" , (req, res) => {
     }
 }
 );
+
+// add product to the products list
+
+app.post("/api/v1/products", (req, res) => {
+    const {body} = req;
+    const newProduct = {
+        id : products[products.length - 1].id + 1,
+        ...body
+    }
+    products.push(newProduct);
+    res.json(products);
+});
+
+// update product by id parameter and body but must all fields be updated
+
+app.put("/api/v1/products/:id", (req, res) => {
+    const {
+        body,
+        params: {id}
+    } = req;
+    const parsedID = parseInt(id);
+    if(isNaN(parsedID)){
+        res.status(400).send('Invalid ID');
+    }
+
+    const findIndexProduct = products.findIndex((product) => product.id === parsedID);
+    if(findIndexProduct === -1){
+        res.status(404).send('Product not found');
+    }
+    products[findIndexProduct] = {
+        id : parsedID,
+        ...body
+    };
+    res.sendStatus(200);
+});
+
+// update specific product attribute by id parameter
+
+app.patch("/api/v1/products/:id", (req, res) => {
+    const {
+        body,
+        params: {id}
+    } = req;
+    const parsedID = parseInt(id);
+    if(isNaN(parsedID)){
+        res.status(400).send('Invalid ID');
+    }
+
+    const findIndexProduct = products.findIndex((product) => product.id === parsedID);
+    if(findIndexProduct === -1){
+        res.status(404).send('Product not found');
+    }
+    products[findIndexProduct] = {
+        ...products[findIndexProduct],
+        ...body
+    };
+    res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
