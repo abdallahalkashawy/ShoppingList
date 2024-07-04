5891004b41336134e020642d488b02507171ff5f
Abdallah Ehab <abdallah.alkashawy@gmail.com>
2024-07-04 10:45:44 +0300

    fix: promocode Id unique value

--- src/handlers/promocodes.mjs
+++ src/handlers/promocodes.mjs
@@ -15,8 +15,12 @@ export const createPromoCodeHandler = (req, res) => {
     if (promoCode) { 
         return res.status(400).send('Promo code already exists');
     }
+    const promocodeExist = promoCodes.some((promoCode) => promoCode.promoCodeID === body.promoCodeID);
+    if(promocodeExist){
+        return res.status(400).send('Promo code ID already exists');
+    }
     const addedPromoCode = {
-        promoCodeID : promoCodes.length + 1,
+        promoCodeID : body.promoCodeID ? body.promoCodeID : promoCodes.length + 1,
         ...body
     }
     promoCodes.push(addedPromoCode);
@@ -25,15 +29,15 @@ export const createPromoCodeHandler = (req, res) => {
 
 export const deletePromoCodeHandler = (req, res) => {
     const { body } = req;
-    if(!body.promoCodeName){
+    if(!body.promoCodeID){
         return res.status(400).send('must include promoCodeName');
     }
-    const promoCode = promoCodes.find((promoCode) => promoCode.promoCodeName === body.promoCodeName);
+    const promoCode = promoCodes.find((promoCode) => promoCode.promoCodeID === body.promoCodeID);
     if (!promoCode) { 
         return res.status(404).send('Promo code not found');
     }
-    const promoCodeIndex = promoCodes.findIndex((promoCode) => promoCode.promoCodeName === body.promoCodeName);
-    if(shoppingList.promoCodes[0] === promoCode.promoCodeName){
+    const promoCodeIndex = promoCodes.findIndex((promoCode) => promoCode.promoCodeID === body.promoCodeID);
+    if(shoppingList.promoCodes[0].promoCodeID === promoCode.promoCodeID){
         shoppingList.totalPrice = shoppingList.totalPrice / (1 - (promoCode.promoCodePercentage / 100));
         shoppingList.promoCodes=[];
     }
@@ -43,10 +47,10 @@ export const deletePromoCodeHandler = (req, res) => {
 
 export const applyPromoCodeHandler = (req, res) => {
     const { body } = req;
-    if(!body.promoCodeName){
+    if(!body.promoCodeID){
         return res.status(400).send('must include promoCodeName');
     }
-    const promoCode = promoCodes.find((promoCode) => promoCode.promoCodeName === body.promoCodeName);
+    const promoCode = promoCodes.find((promoCode) => promoCode.promoCodeID === body.promoCodeID);
     if (!promoCode) { 
         return res.status(404).send('Promo code not found');
     }
@@ -58,7 +62,7 @@ export const applyPromoCodeHandler = (req, res) => {
         return res.status(400).send('Shopping list is empty');
     }
     shoppingList.totalPrice = shoppingList.totalPrice - (shoppingList.totalPrice * promoCode.promoCodePercentage / 100);
-    shoppingList.promoCodes.push(promoCode.promoCodeName);
+    shoppingList.promoCodes.push(promoCode);
    return res.json(shoppingList);
 }
 
@@ -66,7 +70,7 @@ export const removePromoCodeFromShoppingListHandler =  (req, res) => {
     if (shoppingList.promoCodes.length === 0) { 
         return res.status(404).send('Promo Not Applied');
     }
-    const promoCode = promoCodes.find((promoCode) => promoCode.promoCodeName === shoppingList.promoCodes[0]);
+    const promoCode = promoCodes.find((promoCode) => promoCode.promoCodeID === shoppingList.promoCodes[0].promoCodeID);
     shoppingList.totalPrice = shoppingList.totalPrice / (1 - (promoCode.promoCodePercentage / 100));
     shoppingList.promoCodes=[];
    return res.json(shoppingList);
